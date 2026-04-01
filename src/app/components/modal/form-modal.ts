b6df0df9d131d8de63fa104d67aa209fbb2f1e59
arunandholly27 <arunandholly@gmail.com>
2026-03-31 23:19:34 -0400

    modal styling and form changes

--- src/app/components/modal/form-modal.ts
+++ src/app/components/modal/form-modal.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { EntryForm } from '../entry-form/entry-form';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-form-modal',
-  imports: [EntryForm, ReactiveFormsModule],
+  imports: [EntryForm, ReactiveFormsModule, MatDialogModule, MatIconModule],
   templateUrl: './form-modal.html',
   styleUrl: './form-modal.css',
 })
